0fa1bc6556eb3e0e74f9baabf8ca1c049e52bef9
BishowjitDatta <bishowjitdatta@gmail.com>
2021-02-16 01:45:27 +0600

    area

---
+++ MID_LAB_TASK_PHP_04/area.php
@@ -0,0 +1,10 @@
+<?php
+
+  $length=100;
+  $width=60;
+  $Area=$length*$width;
+  $Perimeter=2*($length+$width);
+  echo "The Area of Rectangle is:".$Area."\n";
+  echo "The Perimeter of Rectangle is:".$Perimeter;
+
+?>
