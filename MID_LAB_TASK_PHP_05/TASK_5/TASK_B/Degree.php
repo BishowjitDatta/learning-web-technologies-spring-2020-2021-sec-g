0db175751f4816cea5cdf1e2f0d134f3b499ab7b
BishowjitDatta <bishowjitdatta@gmail.com>
2021-02-17 02:30:52 +0600

    TASK_5_B

---
+++ MID_LAB_TASK_PHP_05/TASK_5/TASK_B/Degree.php
@@ -0,0 +1,37 @@
+<?php
+
+if(isset($_POST['submit']))
+	{
+
+		$name = $_POST['myname'];
+
+		if($name == ""){
+			echo "null submission";
+		}else{
+			echo "success...";
+		}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form class="" action="" method="post">
+        <fieldset>
+          <legend>Degree</legend>
+          <input type="checkbox" name="myname" value="myname"> SSC
+          <input type="checkbox" name="myname" value="myname"> HSC
+          <input type="checkbox" name="myname" value="myname"> BSc
+          <input type="checkbox" name="myname" value="myname"> MSc
+        </fieldset><br>
+        <input type="submit" name="submit" value="Submit">
+      </form>
+</body>
+</html>
