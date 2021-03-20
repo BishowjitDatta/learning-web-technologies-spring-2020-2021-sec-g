3cce6c17f1f21423ffe5a843637c3c5025e1e13a
BishowjitDatta <bishowjitdatta@gmail.com>
2021-03-20 23:15:21 +0600

    update

--- Final_Lab_Task_01_DB/controller/regCheck.php
+++ Final_Lab_Task_01_DB/controller/regCheck.php
@@ -1,9 +1,8 @@
 <?php
 	session_start();
+	require_once('../model/userModel.php');
 
 	if(isset($_POST['signup'])){
-
-		require_once('db.php');
 
 		$username = $_POST['username'];
 		$password = $_POST['password'];
@@ -16,17 +15,21 @@
 
 			if($password == $repass){
 
-				$user = [	
-							'username'=>$username, 
-							'password'=>$password, 
-							'email'=> $email
+				$user = [
+							'username' => $username,
+							'password' => $password,
+							'email' => $email
+
 						];
 
-				//$_SESSION['username'] = $username;
-				//$_SESSION['password'] = $password;
-				$_SESSION['current_user'] = $user;
+				$status = insertUser($user);
 
-				header('location: ../view/login.html');
+				if($status){
+					header('location: ../view/login.html');
+				}else{
+					echo "error";
+				}
+
 			}else{
 				echo "password & confirm password mismatch..";
 			}
--- Final_Lab_Task_01_DB/model/db.php
+++ Final_Lab_Task_01_DB/model/db.php
@@ -1,47 +1,16 @@
-<?php 
+<?php
 
-	//connection
-	$conn = mysqli_connect('localhost', 'root', '', 'usermgt');
+	$dbuser = 'root';
+	$dbpass = '';
+	$host 	= 'localhost';
+	$dbname	= 'web';
 
-	if($conn){
-		echo "success";
-	}else{
-		die('DB error');
+
+	function getConnection(){
+
+		global $dbname, $dbpass, $dbuser, $host;
+		$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+		return $conn;
 	}
-
-/*	$sql = 'select * from users';
-	$result = mysqli_query($conn, $sql);*/
-
-	/*echo "<table border=1> 
-			<tr>
-				<td>ID</td>
-				<td>Username</td>
-				<td>Email</td>
-				<td>Type</td>
-			</tr>";
-	while ($row = mysqli_fetch_assoc($result)) {
-		
-		echo 	"<tr>
-					<td>{$row['id']}</td>
-					<td>{$row['username']}</td>
-					<td>{$row['email']}</td>
-					<td>{$row['type']}</td>
-				</tr>";
-	}
-	echo "</table>";*/
-
-	//$sql = "select * from users where username='' and password=''"; 
-	
-	//query
-	//$sql = "insert into users values('', 'xyz', 'xyz', 'xyz@gmail.com', 'admin')"; 
-
-
-	//array
-	//$result = mysqli_query($conn, $sql);
-	//print_r($result);
-
-
-	//close
-	mysqli_close($conn)
 
 ?>
