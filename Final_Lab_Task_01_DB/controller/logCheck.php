90cce7f45df08bd3d2abed888e655b6829ca036b
BishowjitDatta <bishowjitdatta@gmail.com>
2021-03-20 15:32:15 +0600

    DB Connection

---
+++ Final_Lab_Task_01_DB/controller/logCheck.php
@@ -0,0 +1,25 @@
+<?php
+	session_start();
+
+	require_once('db.php');
+
+	if(isset($_POST['submit'])){
+
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		if($username == "" || $password == ""){
+			echo "null submission...";
+		}else{
+			$user = $_SESSION['current_user'];
+
+			if($username == $user['username'] && $password == $user['password']){
+				$_SESSION['flag'] = true;
+				header('location: ../view/home.php');
+			}else{
+				echo "invalid user";
+			}
+		}
+
+	}
+?>
